0291fe5058056d68dd513db874a20b2bc4f593a1
tillnow2 <rastogip365@gmail.com>
2026-02-12 14:46:52 +0530

    cache issue change

--- src/api/api.js
+++ src/api/api.js
@@ -6,9 +6,6 @@ const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    'Cache-Control': 'no-cache, no-store, must-revalidate',
-    'Pragma': 'no-cache',
-    'Expires': '0',
   },
 });
 
@@ -17,7 +14,6 @@ api.interceptors.request.use((config) => {
   if (config.method === 'get') {
     config.params = {
       ...config.params,
-      _t: Date.now(),
     };
   }
   return config;
